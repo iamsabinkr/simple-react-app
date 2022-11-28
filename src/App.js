60ccacb73055de1dbe3fbe133193e53c12f1e23c
Sabin Kr Shrestha <57905540+iamsabinkr@users.noreply.github.com>
2022-11-28 17:50:37 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Welcome in <b>simple-react-app</b> page.
+          //Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
